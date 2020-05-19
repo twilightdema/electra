e0f8eb2e3912d81a8523b0306aa79c050fb1b4d1
twilightdema@gmail.com <twilightdema@gmail.com>
2020-05-19 05:47:28 +0000

    Add script for STS

---
+++ 3_run_analyse_glue_sts.sh
@@ -0,0 +1,5 @@
+python3 run_error_analyse.py \
+--data-dir data_dir \
+--model-name electra_base \
+--init-from-checkpoint data_dir/models/electra_base/finetuning_models/sts_model_1 \
+--hparams '{"model_size": "base", "task_names": ["sts"], "write_test_outputs": true, "n_writes_test": 9999 }'
